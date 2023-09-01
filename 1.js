79ff597d775e6861ac0c96c1fef2054ec0a9d43f
MoeezHashmi <139455888+MoeezHashmi@users.noreply.github.com>
2023-09-01 23:47:27 +0500

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,68 @@
+                                       CHAPTER NO 1  STAERT       
+                                       Q NO 1
+
+*var greetingMessage = "hello";
+alert(greetingMessage);*
+
+                                        Q NO 2
+*var greetingMessage = "Welcome to js land..\n Hapay coding!";
+alert(greetingMessage);*
+
+                                         Q No 3
+
+*var greetingMessage = "Welcome to js land....";
+alert(greetingMessage);*
+                                         Q No 4
+
+ var greetingMessage = "";
+   alert(greetingMessage);
+                                      Q No 5
+
+*var greetingMessage = "Hello..i can run js through my web browser's console ";
+ alert(greetingMessage);*
+
+
+                                   CHAPTER NO 1 AND 
+                               
+                                 CHAPTER NO 2 START
+                               
+                                  Q No 1
+
+var greetingMessage ="Dhone Doe";
+alert(greetingMessage);
+
+                                   Q No 2
+  var greetingMessage ="15 Years old";
+alert(greetingMessage);
+                                   
+
+                                         Q No 3
+  var greetingMessage ="Certified Mobile Application Development";
+  alert(greetingMessage);
+                              
+
+                                         Q No 4
+
+  var greetingMessage ="PIZZA\nPIZZ\nPIZ\nPI\nP";
+  alert(greetingMessage);
+
+                                          Q No 5
+
+  var greetingMessage ="email adress ak5589667@gmail.com";
+  alert(greetingMessage);
+
+                                       Q No 6
+
+var greetingMessage ="i am trying to learn form the BOOk A Smarter\nWay to learn javascript";
+alert(greetingMessage);
+
+                                       Q No 7
+
+var greetingMessage ="Yah! i can write HTML content through javasscript";~
+alert(greetingMessage);
+
+                                      Q No 8
+
+ var greetingMessage ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+  alert(greetingMessage);
+                                      
